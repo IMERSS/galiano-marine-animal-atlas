c629e6a5cf07ab9da588029828fcdb4c80dbfe15
chlorophilia <andrewsimon@MacBook-Air.lan>
2023-04-22 13:53:36 -0700

    create template R Markdown doc summarizing marine mollusc diversity documented for Galiano Island

---
+++ scripts/Marine_mollusc_map_script.R
@@ -0,0 +1,55 @@
+# Map Galiano Island Mollusc diversity
+
+# Load libraries
+
+library(dplyr)
+library(leaflet)
+library(raster)
+library(reshape2)
+library(rjson)
+library(scales)
+library(sf)
+library(tmap)
+library(viridis)
+
+# Source dependencies
+
+source("scripts/utils.R")
+
+# Read gridded marine mollusc dataset
+
+molluscs.gridded <- read.csv("tabular_data/molluscs_records_gridded.csv")
+
+# Summarize mollusc by grid cell and export to JSON file for viz
+
+gridded.molluscs <- molluscs.gridded %>% group_by(cell_id) %>% 
+  summarize(taxa = paste(sort(unique(scientificName)),collapse=", "))
+
+write(jsonlite::toJSON(gridded.molluscs ), "viz_data/molluscGridCellData.json")
+
+# Load choropleth
+
+choropleth <- mx_read("spatial_data/vectors/molluscs")
+
+# Plot choropleth
+
+tmap_mode("view")
+
+animal.grid.map = tm_shape(choropleth) +
+  tm_fill(
+    col = "richness",
+    palette = "viridis",
+    style = "cont",
+    title = "Richness",
+    showNA = FALSE,
+    alpha = 0.5,
+    popup.vars = c(
+      "Richness: " = "richness"
+    ),
+    popup.format = list(
+      richness = list(format = "f", digits = 0)
+    )
+  ) +
+  tm_borders(col = "grey40", lwd = 0.7)
+
+animal.grid.map
